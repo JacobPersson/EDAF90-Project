eea44da476899ce7e485aaf26adc3b260378398a
JacobPersson <ja8481pe-s@student.lu.se>
2022-02-28 16:22:12 +0100

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SearchComponent } from './search/search.component';
+import { ProfileComponent } from './profile/profile.component';
+import { WatchlistComponent } from './watchlist/watchlist.component';
+import { MovieInformationComponent } from './movie-information/movie-information.component';
+
+const routes: Routes = [
+  { path: 'search', component: SearchComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'movieinformation', component: MovieInformationComponent},
+  { path: 'watchlist', component: WatchlistComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { MovieInformationComponent } from './movie-information/movie-information.component';
+import { WatchlistComponent } from './watchlist/watchlist.component';
+import { ProfileComponent } from './profile/profile.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    MovieInformationComponent,
+    WatchlistComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
